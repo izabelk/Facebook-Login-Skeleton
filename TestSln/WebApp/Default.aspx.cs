e5f9700169728409c9d1fb36becea87e6428cf12
izabelk <izi_girl@abv.bg>
2014-12-03 21:11:07 +0200

    added fb post to wall functionality

--- TestSln/WebApp/Default.aspx.cs
+++ TestSln/WebApp/Default.aspx.cs
@@ -1,6 +1,10 @@
-﻿using System;
+﻿using Facebook;
+using System;
 using System.Collections.Generic;
+using System.Dynamic;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -12,6 +16,56 @@ namespace WebApp
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void ButtonFb_Click(object sender, EventArgs e)
+        {
+            CheckAuthorization();
+        }
+
+        private void CheckAuthorization()
+        {
+            string app_id = "795170443855153";
+            string app_secret = "f378d2e335b78a7bd5da1915af0ea657";
+            string scope = "publish_stream, publish_actions";
+
+            if (Request["code"] == null)
+            {
+                Response.Redirect(string.Format(
+                    "https://graph.facebook.com/oauth/authorize?client_id={0}&redirect_uri={1}&scope={2}",
+                    app_id, Request.Url.AbsoluteUri, scope));
+            }
+            else
+            {
+                Dictionary<string, string> tokens = new Dictionary<string, string>();
+
+                string url = string.Format("https://graph.facebook.com/oauth/access_token?client_id={0}&redirect_uri={1}&scope={2}&code={3}&client_secret={4}",
+                    app_id, Request.Url.AbsoluteUri, scope, Request["code"].ToString(), app_secret);
+
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+
+                var response = request.GetResponse();
+
+                //using (HttpWebResponse response = request.GetResponse() as H)
+                //{
+                    StreamReader reader = new StreamReader(response.GetResponseStream());
+
+                    string vals = reader.ReadToEnd();
+
+                    foreach (string token in vals.Split('&'))
+                    {
+                        //meh.aspx?token1=steve&token2=jake&...
+                        tokens.Add(token.Substring(0, token.IndexOf("=")),
+                            token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+                    }
+                //}
+
+                string access_token = tokens["access_token"];
+
+                var client = new FacebookClient(access_token);
+                
+                client.Post("/me/feed", new { message = "Last test!", edit = true });
+            }
         }
     }
 }
