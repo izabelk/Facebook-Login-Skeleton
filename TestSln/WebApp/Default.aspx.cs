5a01f6c0296eda1a47735fb7e4e7439a9111d25a
izabelk <izi_girl@abv.bg>
2014-12-03 21:55:02 +0200

    tested another library

--- TestSln/WebApp/Default.aspx.cs
+++ TestSln/WebApp/Default.aspx.cs
@@ -1,4 +1,6 @@
 ﻿using Facebook;
+using ImpactWorks.FBGraph.Core;
+using ImpactWorks.FBGraph.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Dynamic;
@@ -25,6 +27,54 @@ namespace WebApp
 
         private void CheckAuthorization()
         {
+            ////Setting up the facebook object
+            //ImpactWorks.FBGraph.Connector.Facebook facebook = new ImpactWorks.FBGraph.Connector.Facebook();
+            //facebook.AppID = "795170443855153";
+            //facebook.CallBackURL = "http://localhost:55534/Default.aspx";
+            //facebook.Secret = "f378d2e335b78a7bd5da1915af0ea657";
+
+            ////Setting up the permissions
+            //List<FBPermissions> permissions = new List<FBPermissions>() {
+ 
+            //    FBPermissions.read_stream,
+ 
+            //    FBPermissions.publish_stream //Permission to publish on wall
+            //};
+
+            ////Pass the permissions object to facebook instance
+            //facebook.Permissions = permissions;
+
+            //if (String.IsNullOrEmpty(Request.QueryString["code"]))
+            //{
+            //    String authLink = facebook.GetAuthorizationLink();
+            //    Response.Redirect(authLink);
+            //}
+            //else
+            //{
+            //    //Get the code returned by facebook
+            //    string Code = Request.QueryString["code"];
+
+            //    //process code for auth token
+            //    facebook.GetAccessToken(Code);
+
+            //    //Get User info
+            //    FBUser currentUser = facebook.GetLoggedInUserInfo();
+            //    //Image share
+            //    //initiating object of type IFeedPost for posting on facebook wall
+            //    IFeedPost FBpost = new FeedPost();
+
+            //    FBpost.Caption = "Image Share";
+
+            //    FBpost.ImageUrl = "http://www.theadway.com/wall.jpg";
+
+            //    FBpost.Name = "Great Image";
+            //    FBpost.Url = "http://www.theadway.com/wall.jpg";
+
+            //    var postId = facebook.PostToWall(currentUser.id.GetValueOrDefault(), FBpost);
+
+            //}
+
+
             string app_id = "795170443855153";
             string app_secret = "f378d2e335b78a7bd5da1915af0ea657";
             string scope = "publish_stream, publish_actions";
@@ -48,23 +98,23 @@ namespace WebApp
 
                 //using (HttpWebResponse response = request.GetResponse() as H)
                 //{
-                    StreamReader reader = new StreamReader(response.GetResponseStream());
+                StreamReader reader = new StreamReader(response.GetResponseStream());
 
-                    string vals = reader.ReadToEnd();
+                string vals = reader.ReadToEnd();
 
-                    foreach (string token in vals.Split('&'))
-                    {
-                        //meh.aspx?token1=steve&token2=jake&...
-                        tokens.Add(token.Substring(0, token.IndexOf("=")),
-                            token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
-                    }
+                foreach (string token in vals.Split('&'))
+                {
+                    //meh.aspx?token1=steve&token2=jake&...
+                    tokens.Add(token.Substring(0, token.IndexOf("=")),
+                        token.Substring(token.IndexOf("=") + 1, token.Length - token.IndexOf("=") - 1));
+                }
                 //}
 
                 string access_token = tokens["access_token"];
 
                 var client = new FacebookClient(access_token);
-                
-                client.Post("/me/feed", new { message = "Last test!", edit = true });
+
+                client.Post("/me/feed", new { message = "Didi e maimunqk!" });
             }
         }
     }
